89ef825dd786a5c8e1aba3ec1979b5bff0a1a23e
Leo Nugraha <32232486+lnugraha@users.noreply.github.com>
2021-03-03 16:39:32 +0800

    Update test.py

--- test.py
+++ test.py
@@ -9,3 +9,12 @@ def test_area_rect_1():
 
 def test_area_rect_2():
    assert area_rect(2.5,4) == 10.0
+
+def test_file1_method1():
+   x=5; y=6
+   assert x+1 == y,"test failed"
+   assert x == y,"test failed"
+
+def test_file1_method2():
+   x=5; y=6
+   assert x+1 == y,"test failed" 
